a6ab5b71d158c9f5b9831ad3657049303b632817
Daniel Aronson <daronson@u.rochester.edu>
2019-03-01 14:26:05 -0500

    changes
    changes

--- README.md
+++ README.md
@@ -1,41 +1,9 @@
-# Assignment 4
+# Assignment 6
 
-## Domain
-The domain of the website is fashion. Each webpage covers a different aspect of fashion: shoes, high-street fashion, and designer houses.
+## Typography
+I picked these fonts because they look clean with the white background. Most fashion stuff wants to portay a clean mood and I feel that these do that well.
 
-## Ontology
-### Overall Website
-- Each webpage has a focus on things related to people 
-### Index
-- Each webpage has a list of topics
-- Each webpage has a creative description
-### Individual Page
-- Each topic has an introduction
-- Each topic has a brief history
+## Readability
+I added a bit more line height to make it more readable. I also adjusting the text-size for some headers to make them stand out more. I decreased the length of a few paragraphs in order to make them all similar length to help the readability. Last, I darkened the colors a bit in order to make them stand out against the background a bit more. 
 
-## Taxonomy 
-### Index
-- h1 tag for "Fashion Facts"
-	- nav bar is "Home, Shoes, High-Street Fashion, Designer House"
 
-	- H2 tag is the name of a webpage
-		- List of subtopics
-		- Description for topic
-
-	- footer is "© CSC 174: Advanced Front-end Web Design and Development"
-
-### Individual Pages
-- h1 tags for "Fashion Facts"
-	- nav bar is "Home, Shoes, High-Street Fashion, Designer House"
-
-	- h2 tag is the name of the subtopic
-		- Introduction
-		- Brief History
-
-	- footer is "© CSC 174: Advanced Front-end Web Design and Development"
-
-## Choreography
-1. Homepage (index page)
-2. Shoes
-3. High-street fashion
-4. Designer house
--- assignment_04/index.php
+++ assignment_04/index.php
@@ -1,62 +1,67 @@
-<?php $currentPage="";
+<?php
+$currentPage="High-Street Fashion | ";
 include "inc/html-top.inc";?>
-			
 
-
-		<section class="excerpt">
-			<div class="text">
-				<h2 class = "fix"><a href="shoes.php">Fashionable Shoes</a></h2>
-
-				<ul>
-					<li>Sneakers</li>
-					<li>Boots</li>
-					<li>Sandals</li>
-				</ul>
-
-				<p>A shoe is an item of footwear intended to protect and comfort the human foot while the wearer is doing various activities. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with appearance originally being tied to function. Additionally, fashion has often dictated many design elements, such as whether shoes have very high heels or flat ones</p>
-
-			</div>
-			<a href="shoes.php"><img src="images/nike.jpg"
-		    	 alt="a picture about shoes"></a>
-		</section>
-
-		<section class="excerpt">
-			<div class="text">
-
-				<h2 class = "fix"><a href="sfashion.php">High-Street Fashion</a></h2>
-
-				<ul>
-					<li>Everlane</li>
-					<li>Forever 21</li>
-					<li>American Eagles</li>
-				</ul>
-
-				<p>Fame. Wealth. Fashion. Once unattainable by any except for the social elite, these characteristics were envied by all. However, with the development of high-street fashion, these companies with humble, family-owned beginnings now allow people to experience fashion in a way never before seen. Everlane is a United States clothing retailer that sells primarily online. The organization is headquartered in San Francisco, California and also has a store in New York City. Forever 21 is an American fast fashion retailer headquartered in Los Angeles, California. American Eagle Outfitters, Inc. is an American lifestyle clothing and accessories retailer.</p>
-
-			</div>
-			<a href="sfashion.php"><img src="images/sfashion.jpg"
-			     alt="a picture about high street fashion"></a>
-		</section>
-
-		<section class="excerpt">
-			<div class="text">
-				<h2 class = "fix"><a href="designerhouse.php">Designer House</a></h2>
-
-				<ul>
-					<li>Louis Vuitton</li>
-					<li>Chanel</li>
-					<li>Dior</li>
-				</ul>
-
-				<p>Fashion isn't created over night, and neither are the companies that make it. Designer houses have a unique history of innovation and inspiration representative of humans ingenuity and creativity. They play a big role in the creation of style and in tern help shape cultures. The Louis Vuitton label was founded by Vuitton in 1854 on Rue Neuve des Capucines in Paris, France. The House of Chanel (Chanel S.A.) originated in 1909 when Gabrielle Chanel opened a millinery shop at 160 Boulevard Malesherbes. The House of Dior was established on 16 December 1946 in "a private house"[5] at 30 Avenue Montaigne Paris B.</p>
-			</div>
-			<a href="designerhouse.php"><img src="images/dhouse.jpg"
-			     alt="a picture about designer house"></a>
-		</section>
 		
+
+	
+		<h2>High-Street Fashion</h2>
+		<article>
+			<h3>Everlane</h3>
+				<div class = "grid">
+					<div>
+
+				<h4>Introduction</h4>
+					<p>Everlane is a United States clothing retailer that sells primarily online. The organization is headquartered in San Francisco, California and also has a store in New York City.</p>
+				</div>
+
+				<div>
+				<h4>Brief History</h4>
+					<p>Everlane was founded in 2010 by Michael Preysman and Jesse Farmer as an online menswear retailer. By 2015 the company had received $1.1 million in seed funding. Everlane started with a referral invite list that gained 60,000 subscribers in 5 days although the company only had 1,500 T-shirts in inventory. In 2012, Everlane opened a pop-up shop in New York for two weeks during the holidays.</p>
+				</div>
+			</div>
+
+		</article>
+		<article>
+			<h3>Forever 21</h3>
+
+				<div class = "grid">
+					<div>
+
+				<h4>Introduction</h4>
+					<p>Forever 21, stylized as FOREVER 21, is an American fast fashion retailer headquartered in Los Angeles, California. Forever 21 began as the store called Fashion 21 with 900 square feet (84 m2) in Highland Park, Los Angeles, in 1984, and has grown into the clothing lines Forever 21, XXI Forever, Love 21 and Heritage with over 600 stores in the Americas, Asia, the Middle East and the UK.</p>
+				</div>
+
+				<div>
+				<h4>Brief History</h4>
+					<p>Originally known as Fashion 21, the store was founded in Los Angeles on 21 April 1984 by husband and wife, Do Won Chang and Jin Sook Chang from Korea. The store is located at 5637 N. Figueroa Street in the Highland Park district of Los Angeles and is still in operation, bearing the chain's original name. Designs similar to those seen in South Korea were sold to and targeted at the Los Angeles Korean American community. In its first year in operation, sales totaled $700,000 and, by 2013, there were more than 480 stores and a revenue of $3.7 billion. In February 2014, Forever 21 generated a revenue of $3.8 billion and in 2017, Forever 21 generated a revenue of $3.4 billion. Originally, Forever 21 only sold clothes for women but later expanded to sell menswear.</p>
+				</div>
+			</div>
+				
+		</article>
+
+		<article>
+			<h3>American Eagle</h3>
+
+				<div class = "grid">
+					<div>
+
+				<h4>Introduction</h4>
+					<p>American Eagle Outfitters, Inc., now known as simply American Eagle, is an American lifestyle clothing and accessories retailer, headquartered in the Southside Works Neighborhood of Pittsburgh, Pennsylvania. American Eagle's beginning was with the Silverman family, which owned and operated Silvermans Menswear.</p>
+				</div>
+
+				<div>
+				<h4>Brief History</h4>
+					<p>By the mid-1970s, two of the Silverman brothers—from the third generation of Silvermans in the family business—were running the family business. Jerry Silverman was the president and CEO, while his brother, Mark Silverman, served as executive vice-president and COO. The Silverman brothers were convinced they needed to diversify their product offerings in order to continue growing their company. They also recognized that the addition of new family-owned chains would then enable them to operate more than one store in the same mall. Their first attempt was to open American Eagle Outfitters in 1977, positioning it as a proprietor of brand-name leisure apparel, footwear, as well as accessories for men and women, emphasizing merchandise suited for outdoor sports, such as hiking, mountain climbing, and camping.</p>
+				</div>
+			</div>
+				
+			
+		</article>
 	</main>
 
-	<footer> © CSC 174: Advanced Front-end Web Design and Development</footer>	
+	<footer> © CSC 174: Advanced Front-end Web Design and Development</footer>
+
 
     <?php include "inc/scripts.inc"; ?>
 
